e763f30c22d906d367caaa442990c48898d930bc
AlexFDRK <alex.fdrk@gmail.com>
2022-01-15 12:45:45 -0500

    15-01.12:45

--- scripts/main.js
+++ scripts/main.js
@@ -28,78 +28,91 @@ const initialCards = [
     }
 ];
 
-function addElement(name, link){
-    const cardElement = elementTemplate.cloneNode(true);
-  
-    cardElement.querySelector('.element__picture').src = link;
-    cardElement.querySelector('.element__picture').alt = name;
-    cardElement.querySelector('.element__name').textContent = name;
-    cardElement.querySelector('.element__like').addEventListener('click', function (event) {
-      event.target.classList.toggle('element__like_state_active');
-    });
-    cardElement.querySelector('.element__bin').addEventListener('click', function (event) {
-      const elementItem = event.target.closest('.element');
-      elementItem.remove();
-    });
-    return cardElement;
-}
-
-initialCards.forEach(function (element) {
-    sectionElements.append(addElement(element.name, element.link));
-});
-
 /*============================================================*/
 
-const btnClose = document.querySelector('.popup__close');
 const btnEdit = document.querySelector('.profile__edit-button');
 const btnAdd = document.querySelector('.profile__add-button');
 const popup = document.querySelector('.popup');
-const popupActive = 'popup_active';
-const formActive = 'form__active';
+
+const aimName = document.querySelector('.profile__name');
+const aimDescription = document.querySelector('.profile__description');
 
 const active = 'element__like_state_active';
 const disabled = 'element__like_state_disabled';
+const content = document.querySelector('.content');
+const btnLike = document.querySelector('.element__like');
 
-let content = document.querySelector('.content');
-let btnLike = document.querySelector('.element__like');
+let activeOverlay;
+let btnClose;
+const popupActive = 'active';
 
-let formMyForm = document.querySelector('.my-form');
-let strName = formMyForm.querySelector('.form__field_text_name');
-let strDescription = formMyForm.querySelector('.form__field_text_description');
-const btnSave = formMyForm.querySelector('.form__save_frm_button');
+const firstOverlay = document.querySelector('#first-overlay');
+const strName = firstOverlay.querySelector('.form__field_text_name');
+const strDescription = firstOverlay.querySelector('.form__field_text_description');
+const btnSave = firstOverlay.querySelector('.form__save_frm_button');
 
-let formAddForm = document.querySelector('.add-form');
-let strAddName = formAddForm.querySelector('.form__field_text_name');
-let strAddDescription = formAddForm.querySelector('.form__field_text_description');
-const btnCreate = formAddForm.querySelector('.form__create_frm_button');
+const secondOverlay = document.querySelector('#second-overlay');
+const strAddName = secondOverlay.querySelector('.form__field_text_name');
+const strAddDescription = secondOverlay.querySelector('.form__field_text_description');
+const btnCreate = secondOverlay.querySelector('.form__create_frm_button');
 
-let aimName = document.querySelector('.profile__name');
-let aimDescription = document.querySelector('.profile__description');
+const imgViewPicture = document.querySelector('.view__picture');
+const strViewName = document.querySelector('.view__picture-name');
+
+function setActiveElements(selectorName){
+  activeOverlay = document.querySelector(selectorName);
+  activeOverlay.classList.add(popupActive);
+  btnClose = activeOverlay.querySelector('.popup__close');
+  btnClose.addEventListener('click', function(){
+    popupClose();
+  });
+}
+
+function addElement(name, link){
+  const cardElement = elementTemplate.cloneNode(true);
+
+  cardElement.querySelector('.element__picture').src = link;
+  cardElement.querySelector('.element__picture').alt = name;
+  cardElement.querySelector('.element__name').textContent = name;
+  cardElement.querySelector('.element__like').addEventListener('click', function (event) {
+    event.target.classList.toggle('element__like_state_active');
+  });
+  cardElement.querySelector('.element__bin').addEventListener('click', function (event) {
+    const elementItem = event.target.closest('.element');
+    elementItem.remove();
+  });
+  cardElement.querySelector('.element__picture').addEventListener('click', function (event) {
+    popup.classList.add(popupActive);
+    setActiveElements('#third-overlay');
+    imgViewPicture.src = event.target.src;
+    strViewName.textContent = event.target.alt;
+  });
+  return cardElement;
+}
+
+initialCards.forEach(function (element) {
+  sectionElements.append(addElement(element.name, element.link));
+});
 
 btnEdit.addEventListener('click', function(){
-    popup.classList.add(popupActive);
-    formMyForm.classList.add(formActive);
-    strName.value = aimName.textContent;
-    strDescription.value = aimDescription.textContent;
+  popup.classList.add(popupActive);
+  setActiveElements('#first-overlay');
+  strName.value = aimName.textContent;
+  strDescription.value = aimDescription.textContent;
 });
 
 btnAdd.addEventListener('click', function(){
     popup.classList.add(popupActive);
-    formAddForm.classList.add(formActive);
+    setActiveElements('#second-overlay');
 });
 
 function popupClose(){
     popup.classList.remove(popupActive); 
-    formMyForm.classList.remove(formActive);
-    formAddForm.classList.remove(formActive);
+    activeOverlay.classList.remove(popupActive);
 }
 
-btnClose.addEventListener('click', function(){
-    popupClose();
-});
-
 document.addEventListener('keydown', function(event){
-    if (event.сode === 'Escape'){
+    if (event.key === 'Escape'){
         popupClose();
     }
 });
@@ -124,5 +137,5 @@ function formAddSubmitHandler (event){
     popupClose();
 }
 
-formMyForm.addEventListener('submit', formSubmitHandler);
-formAddForm.addEventListener('submit', formAddSubmitHandler);
+firstOverlay.addEventListener('submit', formSubmitHandler);
+secondOverlay.addEventListener('submit', formAddSubmitHandler);
